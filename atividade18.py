0aca07accf67be4e4faf65a8d7c4158cf066871d
KleberSoaresdev <143618629+KleberSoaresdev@users.noreply.github.com>
2023-10-10 11:02:15 -0300

    Update atividade18.py

--- atividade18.py
+++ atividade18.py
@@ -3,3 +3,10 @@ b = []
 for y in range(6,-1,-1):
     b.append(a[y])
 print(b)
+#correção da atividade em sala:
+a = [2, 5, 4, 2, 8, 5, 2]
+b = [0, 0, 0, 0, 0, 0, 0]
+
+for x in range(7):
+    b[6-x] = a[x]
+print(b)
