118eb9f25f5936a1b27eeff003bc2967c691c02b
KleberSoaresdev <143618629+KleberSoaresdev@users.noreply.github.com>
2023-10-10 11:22:59 -0300

    Update atividade19.py

--- atividade19.py
+++ atividade19.py
@@ -6,3 +6,14 @@ while contagem <11:
     impar += 2
     contagem +=1
 print(saida)
+#correção da atividade em sala:
+c = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+impar = 0
+cont = 0
+while True:
+    if cont%2 !=0:
+        c[impar]=cont
+        impar=impar+1
+    cont=cont+2
+    if impar >=10:
+        break
